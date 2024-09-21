5df7d166ef2199db31ce5e222d1939883e05101f
Ivan <gordeevid@yandex.ru>
2024-09-21 16:57:18 +0300

    fix test

--- tests/test_yandex_disk.py
+++ tests/test_yandex_disk.py
@@ -16,17 +16,23 @@ class TestYDCreateFolder:
             'path': 'TestFolder'
         }
 
-    def __test_YD(self):
-        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
-                                params=self.params,
-                                headers=self.headers)
-        return response.status_code
+    def __create_folder(self):
+        try:
+            response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
+                                    params=self.params,
+                                    headers=self.headers)
+            return response.status_code
+        except requests.exceptions.ConnectionError:
+            return 404
+        
 
     def test_create_folder(self):
-        result = self.__test_YD()
+        result = self.__create_folder()
         if result == 409:
             self.teardown_method()
-            result = self.__test_YD()
+            result = self.__create_folder()
+        if result == 404:
+            pytest.skip('Connction Error! Check internet connection')
         assert result == 201
     
     def teardown_method(self):
